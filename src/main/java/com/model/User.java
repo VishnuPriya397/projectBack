a8012eeaaaa123aa155bfd9707cc2472e05f0bb0
Priya <Priya@192.168.1.5>
2018-01-15 08:06:56 +0530

    validation

--- src/main/java/com/model/User.java
+++ src/main/java/com/model/User.java
@@ -5,7 +5,11 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.Email;
 import org.springframework.stereotype.Component;
 
 @Entity
@@ -19,11 +23,24 @@ public class User implements  Serializable
 	@Id
 	@GeneratedValue
 	private int id;
+	
+	@NotNull(message="please enter the username")
+	@Size(min=5,max=10)
 	private String username;
+	
+	@NotNull(message="please enter the email")
+	@Email
 	private String email;
-	private boolean enable;
-
+	
+	@NotNull(message="please enter the password")
+	@Size(min = 6, max = 15)
 	private String password;
+	
+	@NotNull(message="please enter the contact")
+	@Pattern(regexp="(^$|[0-9]{10})")
+	private int contact;
+
+	private boolean enable;
 	private String role;
 
 	public boolean isEnable() {
@@ -42,8 +59,7 @@ public class User implements  Serializable
 		this.role = role;
 	}
 
-	private int contact;
-
+	
 	public int getId() {
 		return id;
 	}
@@ -68,16 +84,6 @@ public class User implements  Serializable
 		this.email = email;
 	}
 
-    
-
-/*	public boolean isEnable() {
-		return enable;
-	}
-
-	public void setEnable(boolean enable) {
-		this.enable = enable;
-	}*/
-
 	public String getPassword() {
 		return password;
 	}
@@ -86,14 +92,6 @@ public class User implements  Serializable
 		this.password = password;
 	}
 
-	/*public String getRole() {
-		return role;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-*/
 	public int getContact() {
 		return contact;
 	}
@@ -102,7 +100,5 @@ public class User implements  Serializable
 		this.contact = contact;
 	}
     
-	
-	
 }	
 	
